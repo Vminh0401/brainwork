fb137048a1ef7d40001891cf20f85a5b7dbd8848
Vminh0401 <vminh04012000@gmail.com>
2023-10-27 13:32:53 +0700

    Create Login and Signin function

--- customadmin/urls.py
+++ customadmin/urls.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
-from .views import *
+from customadmin import views
+app_name = 'custom_admin'
 
 urlpatterns = [
-    path('', admin_login, name='admin_login'),
-    path('admin_page/', admin_page, name='admin_page'),
-    path('sign_up/', admin_register, name='sign_up'),
+    path('', views.admin_login, name='admin_login'),
+    path('admin_page/', views.admin_page, name='admin_page'),
+    path('sign_up/', views.sign_up, name='sign_up'),
 
 ]
--- customadmin/views.py
+++ customadmin/views.py
@@ -1,17 +1,47 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 
 
 # Create your views here.
+def sign_up(request):
+    if request.method == "POST":
+        username = request.POST.get('username')
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+        cpassword = request.POST.get('cpassword')
+
+        if password != cpassword:
+            messages.success(request, 'Sai mk')
+            return render(request, '../templates/admin_register/Admin_signup.html')
+
+        else:
+            user_admin = User.objects.create_superuser(username, email, password)
+            user_admin.save()
+            return render(request, '../templates/admin_login/Admin_login.html')
+    return render(request, '../templates/admin_register/Admin_signup.html')
+
 
 def admin_login(request):
-    return render(request, '../templates/admin_login/Admin_login.html')
+    if request.method == "POST":
+        username = request.POST['username']
+        password = request.POST['password']
 
-
-def admin_register(request):
-    return render(request, '../templates/admin_register/Admin_signup.html')
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return render(request, '../templates/admin_page/Admin_page.html')
+        else:
+            messages.success(request, 'Try Again!!')
+            return render(request, '../templates/admin_login/Admin_login.html')
+    else:
+        return render(request, '../templates/admin_login/Admin_login.html')
 
 
 def admin_page(request):
     return render(request, '../templates/admin_page/Admin_page.html')
+
+
+def sign_out(request):
+    pass
